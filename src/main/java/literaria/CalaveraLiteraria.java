3869f0db05828e189a54ca08a198f7154543f135
MCC-Maritza <M.C.C.Maritza@outlook.es>
2022-11-06 23:40:58 -0600

    Se realizan modifiaciones

--- src/main/java/literaria/CalaveraLiteraria.java
+++ src/main/java/literaria/CalaveraLiteraria.java
@@ -1,8 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package literaria;
+
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
@@ -15,77 +12,76 @@ import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 
 public class CalaveraLiteraria extends JFrame {
-   public JPanel panel;
- 
-    public CalaveraLiteraria(){
-    setBounds(100,200,500,500);
-    setDefaultCloseOperation(EXIT_ON_CLOSE);// SERRAR VENTANA
-    setTitle("Calaverita Literaria");// Titulo de la ventana
-    setLocationRelativeTo(null);//estableccemos la ventana en el centro de la pantalla
-    this.getContentPane().setBackground(Color.ORANGE);
-    iniciarComponentes();
-}
-private void iniciarComponentes(){
-   colocarPaneles();
-//colocarEtiquetas();
-colocarBotones();
-}
-private void colocarPaneles(){
-panel =new JPanel();//Creacion de un panel
-    this.getContentPane().add(panel);
-    colorcarEtiquetas();
-}
-private void colorcarEtiquetas(){
-    
-    panel.setBackground(Color.BLACK);//Color al panel
-    JLabel etiqueta=new JLabel("Tayler feliz sonrefa\n" +
-"queriendo la escula disfrutar\n" +
-"mas la calaca en la mira le tenia\n" +
-"ya se lo queria llevar\n" +
-"\n" +
-"De nada sirvio su llanto\n" +
-"ni sus suplicas de piedad\n" +
-"la calaca se la echo en el manto\n" +
-"porque todas las materia reprobaba ya\n" +
-"");//Etiqueta
-    panel.add(etiqueta);
 
-    etiqueta.setBounds(85, 10, 300, 80);
-    etiqueta.setHorizontalAlignment(SwingConstants.CENTER);
-    etiqueta.setForeground(Color.WHITE); //2stablecenos el
-    etiqueta.setOpaque(true); //Establecenos pintar el fond
-    etiqueta.setBackground (Color.BLACK); //cambianos eb co!
-    etiqueta.setFont(new Font("chiller",0,30));
-    panel.add(etiqueta); //agr Sy ariqueta 42 nape
-    //etiqueta tipo imagen
-    JLabel etiqueta2= new JLabel();
-    etiqueta2.setIcon(new ImageIcon("calavera1.jpg") );
-    panel.add(etiqueta2);
-    etiqueta2.setBounds(30, 90,658, 658);
-}
-private void colocarBotones() {
-JButton boton1 = new JButton();
-boton1.setText ("Puchale"); //establecemos un texto al boton
-boton1.setBounds (100, 100, 100, 40);
-boton1.setEnabled (true); //establecemos el encendido al boton
-boton1.setMnemonic('a'); //alt + a para iniciar el boton
-boton1.setForeground(Color.RED);//color de la letra
-boton1.setFont(new Font("cooper black",3,20));
-boton1.addActionListener(new ActionListener() {
+    public JPanel panel;
 
-
- public void actionPerformed(ActionEvent arg0) {
-
-    CalaveraLiteraria texto = new CalaveraLiteraria();
-    texto.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    texto.setVisible(true);
-
-    setVisible(false);
+    public CalaveraLiteraria() {
+        setSize(385, 500);
+        setTitle("Calaverita Literaria");// Titulo de la ventana
+        setLayout(null);
+        iniciarComponentes();
+        setLocationRelativeTo(null);//estableccemos la ventana en el centro de la pantalla
+        //this.getContentPane().setBackground(Color.ORANGE);
 
     }
+
+    private void iniciarComponentes() {
+
+        /*private void colocarPaneles() {
+        panel = new JPanel();//Creacion de un panel
+        this.getContentPane().add(panel);
+        colorcarEtiquetas();
+        panel.setVisible(true);
+    }*/
+        //panel.setBackground(Color.BLACK);//Color al panel
+        JLabel etiqueta = new JLabel("Tayler feliz sonreia\n"
+                + "queriendo la escula disfrutar\n"
+                + "mas la calaca en la mira le tenia\n"
+                + "ya se lo queria llevar\n"
+                + "\n"
+                + "De nada sirvio su llanto\n"
+                + "ni sus suplicas de piedad\n"
+                + "la calaca se la echo en el manto\n"
+                + "porque todas las materia reprobaba ya\n"
+                + "");//Etiqueta
+        //panel.add(etiqueta);
+
+        etiqueta.setBounds(10, 100, 365, 280);
+        //etiqueta.setHorizontalAlignment(SwingConstants.CENTER);
+        etiqueta.setForeground(Color.WHITE); //2stablecenos el
+        etiqueta.setOpaque(true); //Establecenos pintar el fond
+        etiqueta.setBackground(Color.BLACK); //cambianos eb co!
+        etiqueta.setFont(new Font("chiller", 0, 30));
+        //panel.add(etiqueta); //agr Sy ariqueta 42 nape
+        //etiqueta tipo imagen
+        etiqueta.setVisible(false);
+
+        JLabel etiqueta2 = new JLabel();
+        etiqueta2.setIcon(new ImageIcon("calavera1.jpg"));
+        //panel.add(etiqueta2);
+        etiqueta2.setBounds(10, 10, 400, 200);
+        etiqueta2.setVisible(false);
+
+        JButton boton1 = new JButton();
+        boton1.setText("Puchale"); //establecemos un texto al boton
+        boton1.setBounds(120, 400, 150, 40);
+        //boton1.setEnabled(true); //establecemos el encendido al boton
+        boton1.setMnemonic('a'); //alt + a para iniciar el boton
+        boton1.setForeground(Color.RED);//color de la letra
+        boton1.setFont(new Font("cooper black", 3, 20));
+        boton1.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent arg0) {
+
+                etiqueta.setVisible(true);
+                etiqueta2.setVisible(true);
+
+            }
         });
 
-       add(panel);
+        //add(panel);
+        add(etiqueta);
+        add(etiqueta2);
         add(boton1);
 
     }
--- src/main/java/literaria/Principal.java
+++ src/main/java/literaria/Principal.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package literaria;
 
 import javax.swing.JFrame;
@@ -12,12 +8,11 @@ import javax.swing.JFrame;
  */
 public class Principal {
 
-    
     public static void main(String[] args) {
-       
-CalaveraLiteraria c1= new CalaveraLiteraria();
-c1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-c1.setVisible(true);
- }
-    
+
+        CalaveraLiteraria c1 = new CalaveraLiteraria();
+        c1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        c1.setVisible(true);
+    }
+
 }
